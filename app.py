570c498b046d693711f1fd790001bdc44c3a5272
ruthvik1665 <91338309+ruthvik1665@users.noreply.github.com>
2023-09-04 15:20:00 +0000

    added little front end

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+post=[{'designation':'president','des':'Need to be perfect in managing people,good leadership qualities,profound communication skills'
+      },
+      {'designation':'vice president',
+       'img':' "https://clubjoinme.ruthvikbasetti.repl.co/cs" ','des':'Need to be perfect in managing people,good leadership qualities,profound communication skills'
+      },
+      {'designation':'club member','des':'Need to be perfect in managing people,good leadership qualities,profound communication skills'
+      },
+      {'designation':'club member',
+       'des':'Need to be perfect in managing people,good leadership qualities,profound communication skills'
+      },
+      {'designation':'club member','des':'Need to be perfect in managing people,good leadership qualities,profound communication skills'
+      }]
 @app.route("/")
 def hello_world():
   return render_template('home.html')
 
+@app.route("/cs")
+def cs():
+  return render_template('cs.html',posts=post)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=False)
